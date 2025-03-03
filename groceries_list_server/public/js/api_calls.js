c2234875d8937ec2732113a29c60fe92f01a1f70
Olivier Pothron <olivier.pothron@gmail.com>
2025-03-03 20:37:21 +0100

    Restructured javascript files

---
+++ groceries_list_server/public/js/api_calls.js
@@ -0,0 +1,66 @@
+console.log("'api_calls.js' loaded.");
+
+function addCategory (name) {       // ✓
+  return fetch("http://localhost:3000/api/categories/", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({ newCategoryName: name})
+  })
+  .then(response => {
+    if(!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    return data;
+  })
+  .catch(error => console.error("Error adding category:", error));
+};
+
+function addGrocery (name, categoryId) {  // ✓
+  return fetch("http://localhost:3000/api/groceries/", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({ newGroceryName: name, categoryId: categoryId})
+  })
+  .then(response => {
+    if(!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    return data;
+  })
+  .catch(error => console.error("Error adding grocery:", error));
+}
+
+function toggleToBeBoughtInDB(element) {  // ✓
+  const groceryName = element.dataset.name;
+  const groceryId = element.dataset.id;
+  const toBeBoughtState = element.dataset.toBeBought;
+  const newToBeBoughtState = toBeBoughtState == '0' ? 1 : 0;
+
+  fetch(`http://localhost:3000/api/groceries/${groceryId}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({ toBeBought: newToBeBoughtState })
+  })
+  .then(response => {
+    if (response.ok) {  // Update client side
+      element.dataset.toBeBought = newToBeBoughtState;
+      element.classList.toggle("to-be-bought");
+      userLog(`${groceryName} to_be_bought state updated to ${newToBeBoughtState}`, 'success');
+    } else {
+      console.error("Error updating to_be_bought value!");
+    }
+  })
+  .catch(error => console.error("Error updating grocerie:", error));
+}
--- groceries_list_server/public/js/modal_form.js
+++ groceries_list_server/public/js/modal_form.js
@@ -1,4 +1,4 @@
-console.log("'modal_form.js' loaded")
+console.log("'modal_form.js' loaded");
 
 // DOM ELEMENTS
 
@@ -73,50 +73,6 @@ function addGroceryToCategoryList(groceryObject) {
 
 // LOGIC HELPERS & HANDLERS
 
-//Checks for validation of item name
-const validateItemNameInput = () => {
-  // MAYBE PUT MORE LOGIC INSIDE IT
-  const itemName = formItemName.value;
-  if (itemName) {
-    return { valid: true, name: itemName };
-  } else {
-    return { valid: false, comment: "Item Name required!" };
-  }
-}
-
-//Checks for validation of category input
-const validateCategoryInput = () => {
-
-  const selectedOption = categorySelector.options[categorySelector.selectedIndex];
-
-  if (selectedOption.value === "custom") {
-    const customCatName = customCategoryInput.value;
-
-    // searches for 'custom' category in selector options
-    const selectorOptionsArray = Array.from(categorySelector.options);
-    const isCategoryInSelector = selectorOptionsArray.some((element) =>
-      element.value === customCatName
-    );
-
-    if (!customCatName) { // maybe trim to prevent empty spaces
-      return { valid: true, type: 'empty', value: "" };
-    } else if (isCategoryInSelector) {                                          // maybe try to return valid and
-      return { valid: false, comment: "Category already in selector!"};
-    } else {
-      return { valid: true, type: 'custom', name: customCatName };
-    }
-  } else if (selectedOption.value === "") {
-    return { valid: true, type: 'empty', value: "" };
-  } else {
-    return {
-      valid: true,
-      type: 'regular',
-      name: selectedOption.value,
-      id: selectedOption.dataset.id
-    };
-  }
-}
-
 //Process Form Submission helper
 const processFormSubmission = (itemName, categoryId, categoryName, categoryType) => {
   // console.log(itemName, categoryId, categoryName, categoryType);
@@ -168,37 +124,6 @@ const handleGroceryAddition = (itemName, categoryId) => {
   .catch(error => {
     console.error("Error adding custom category:", error);
   })
-}
-
-// UI HELPERS FUNCTIONS
-function shakeIt() {
-  modalContent.classList.add("shake");
-      modalContent.addEventListener("animationend", () => {
-        modalContent.classList.remove("shake");
-      }, { once: true });
-}
-
-const showValidationIcon = (field, isValid) => {
-  const parentDiv = field.parentElement;
-  const validationIcon = parentDiv.querySelector(".validation");
-  validationIcon.style.display = "inline";
-  validationIcon.classList.add(isValid ? "right" : "wrong");
-  validationIcon.textContent = isValid ? "\u2713" : "\u2A2F";
-}
-
-const hideValidationIcon = (field) => {
-  const parentDiv = field.parentElement;
-  const validationIcon = parentDiv.querySelector(".validation");
-  validationIcon.classList.remove("wrong");
-  validationIcon.classList.remove("right");
-  validationIcon.style.display = "none";
-}
-
-const validationText = (itemValidation, categoryValidation) => {
-  const messages = [];
-  if (!itemValidation.valid) messages.push(itemValidation.comment);
-  if (!categoryValidation.valid) messages.push(categoryValidation.comment);
-  return messages.join(" ");
 }
 
 //Resets form fields and icons on modal display change
